0d5651eb384ad1dc4d9b4ebcfb38fbbab8f832bb
RivkaBarashi2581 <rb0583252581@gmail.com>
2026-05-09 22:27:55 +0300

    feature/server user,preference,auth

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Serve uploaded files
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const authRoutes = require("./src/routes/auth.routes");
 app.use("/api/auth", authRoutes);
